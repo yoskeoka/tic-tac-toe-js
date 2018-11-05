f60ee8a79d11346e7d38287a639bd9669bbda9e8
Sungwoo <kimkim625625@gmail.com>
2018-11-05 18:44:40 +0900

    [add] ### テストケース追加

--- src/__test__/core.test.js
+++ src/__test__/core.test.js
@@ -8,6 +8,12 @@ describe('Core logic', () => {
         test('x--oox--- 空じゃないよ', () => {
             expect(core.parse('x--oox---')).toEqual([-1, 0, 0, 1, 1, -1, 0, 0, 0]);
         });
+        test('ooox-x-x- マルの勝ち', () => {
+            expect(core.parse('ooox-x-x-')).toEqual([1, 1, 1, -1, 0, -1, 0, -1, 0]);
+        });
+        test('xo-ox-o-x バツの勝ち', () => {
+            expect(core.parse('xo-ox-o-x')).toEqual([-1, 1, 0, 1, -1, 0, 1, 0, -1]);
+        });
     });
     describe('string function', () => {});
 });
