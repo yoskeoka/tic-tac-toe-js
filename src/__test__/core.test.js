e624ece44316e6646178cf0607a992aa0a258e18
Sungwoo Kim <s.kim@gemcook.com>
2018-11-06 20:57:47 +0900

    [add] ### convertString作成

--- src/__test__/core.test.js
+++ src/__test__/core.test.js
@@ -14,6 +14,29 @@ describe('Core logic', () => {
         test('xo-ox-o-x バツの勝ち', () => {
             expect(core.parse('xo-ox-o-x')).toEqual([-1, 1, 0, 1, -1, 0, 1, 0, -1]);
         });
+        test('abc------ 変な文字は例外発生', () => {
+            expect(() => core.parse('abc------')).toThrowError();
+        });
+        test('0 数字は例外発生', () => {
+            expect(() => core.parse(0)).toThrowError();
+        });
+        test('null nullは例外発生', () => {
+            expect(() => core.parse(null)).toThrowError();
+        });
+        test('-- 少なければ例外発生', () => {
+            expect(() => core.parse('--')).toThrowError();
+        });
     });
-    describe('string function', () => {});
+
+    describe('convertString function', () => {
+        test('空のボードになる', () => {
+            expect(core.convertString([0, 0, 0, 0, 0, 0, 0, 0, 0])).toEqual('---------');
+        });
+        test('配列じゃないもの', () => {
+            expect(() => core.convertString(0)).toThrowError();
+        });
+        test('マルバツいくつか入った状態', () => {
+            expect(core.convertString([1, 1, 1, -1, 0, -1, 0, -1, 0])).toEqual('ooox-x-x-');
+        });
+    });
 });
--- src/core.js
+++ src/core.js
@@ -11,21 +11,58 @@
 
 /**
  * 文字列をtic-tac-toeの内部データに変換
+ * srt の例  'x--oox---'
  * @param {string} str
  */
 function parse(str) {
-    // srt の例  'x--oox---'
+    if (typeof str !== 'string') {
+        throw new Error('文字入れて');
+    }
+    if (str.length !== 9) {
+        throw new Error('9桁入れて');
+    }
     const result = str.split('').map(s => {
         if (s === 'x') {
             return -1;
         } else if (s === 'o') {
             return 1;
+        } else if (s === '-') {
+            return 0;
         }
-        return 0;
+        throw new Error('xかoか-入れて');
     });
+    return result;
+}
+
+/**
+ * tic-tac-toeの内部データを文字列に変換
+ * boardの例 [0,0,1,0,-1,0,1,-1,0]
+ * @param {Array<number>} board
+ * @returns {string}
+ */
+function convertString(board) {
+    if (!Array.isArray(board)) {
+        throw new Error('配列入れて');
+    }
+    if (board.length !== 9) {
+        throw new Error('9個入れて');
+    }
+    const result = board
+        .map(n => {
+            if (n === 1) {
+                return 'o';
+            } else if (n === -1) {
+                return 'x';
+            } else if (n === 0) {
+                return '-';
+            }
+        })
+        .join('');
+
     return result;
 }
 
 module.exports = {
     parse: parse,
+    convertString: convertString,
 };
